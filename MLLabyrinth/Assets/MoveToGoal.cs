da2902cb2dffc47229d60087a8dc477accb819ac
Megan :) <imafool68@gmail.com>
2021-03-23 16:39:07 -0700

    Completed Labyrinth Unity project

---
+++ MLLabyrinth/Assets/MoveToGoal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public Transform goal;
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.hasPath)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
